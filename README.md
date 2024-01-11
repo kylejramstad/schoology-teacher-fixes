88eb8b15495819d17580c44b1e678207bac9390b
Kyle Ramstad <kylejramstad@gmail.com>
2024-01-11 11:17:44 -0800

    added hide-show button to student submissions

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# Schoology Google Doc Scrolling Fix
-This is an unpacked Chrome extension. For details on how to load an unpacked extension see [Google's Loading an unpacked extension instructions](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+# Schoology Teacher Fixes
+
+This is an unpacked Chrome extension. For details on how to load an unpacked extension see [Google&#39;s Loading an unpacked extension instructions](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+
+This extension fixes two issues for teachers.
+
+* Google Doc Assignment Scrolling Fix
+* Hide/Show student submissions
+
+## Schoology Google Doc Scrolling Fix
 
 This fixes the CSS on the submission page for teachers. Normally the page contains a long list of student submissions that does not scroll independantly. This fixes that so that you can scroll through students without scrolling past the current Google Doc submission.
 
 ### Before
+
 ![before](images/before.png)
+
 ### After
+
 ![after](images/after.png)
+
+## Hide/Show Student Submissions
+
+This adds a hide/show button to student submissions on Schoology assignments. This allows teachers to hide their submissions so that they can present assignment pages without sharing student grading information to the entire class by mistake.
+
+### Example
+
+![show](images/show.png)
+![hide](images/hide.png)
--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,75 @@
-const checking = setInterval(getElement, 500);
-var element;
-var size = 15;
+//Load fixes when DOM is loaded
+document.addEventListener('DOMContentLoaded', fireContentLoadedEvent,false);
 
-function getElement(){
-    const elements = document.getElementsByTagName('ol');
-    element = elements[0];
-    if(element && elements.length == 1 && location.href.indexOf("submissions") > -1){
-        clearInterval(checking);
+function fireContentLoadedEvent () {
+    getSubmissionElement(); //hideShow fix
+    getGoogleAssignmentElement(); //scroll fix
+}
 
-        const doc = document.getElementsByClassName('SubmissionAppContainer')[0];
+//Reload google assignment scroll fix if the user is switching between assignment, in progress, and submission
+const observeUrlChange = () => {
+    let oldHref = document.location.href;
+    const body = document.querySelector("body");
+    const observer = new MutationObserver(mutations => {
+      if (oldHref !== document.location.href) {
+        oldHref = document.location.href;
+        getGoogleAssignmentElement();
+      }
+    });
+    observer.observe(body, { childList: true, subtree: true });
+  };
+  window.onload = observeUrlChange;
 
-        element.style.overflow = "scroll";
-        element.style.height = doc.offsetHeight+'px';
+//Google assignment scroll fix
+function getGoogleAssignmentElement(){
+    if(location.href.indexOf('submissions') > -1){ //have to be on the submissions page because the error only happens here. Not on the inProgress page
+        //Find element
+        const elements = document.getElementsByTagName('ol');
+        const googleAssignmentElement = elements[0];
+
+        if(googleAssignmentElement && elements.length == 1){
+            //Fix element
+            const doc = document.getElementsByClassName('SubmissionAppContainer')[0];
+            googleAssignmentElement.style.overflow = "scroll";
+            googleAssignmentElement.style.height = doc.offsetHeight+'px';
+        }else{
+            setTimeout(getGoogleAssignmentElement,500); //wait and try again because google doc loading may be blocking the fix
+        }
     }
+}
+
+//Hide/Show student submission fix
+var hide = false;
+function getSubmissionElement(){
+    //Find element
+    const rightColumn = document.getElementById('right-column');
+
+    if(rightColumn){
+        //Add button
+        const hideShowBtn = document.createElement('button');
+        hideShowBtn.id = 'hideShowBtn';
+        hideShowBtn.style.padding = '5px';
+        hideShowBtn.style.position = 'relative';
+        hideShowBtn.style.top = '10px';
+        hideShowBtn.style.left = '20px';
+        rightColumn.insertBefore(hideShowBtn, rightColumn.childNodes[0]);
+        hideShowBtn.addEventListener('click',hideShow);
+
+        //Hide button right away by default
+        hideShow();
+    }
+}
+
+//Button function
+function hideShow(){
+    hide = !hide; //flip hide<->show
+    
+    //get elements from page
+    const hideShowBtn = document.getElementById('hideShowBtn');
+    const submissions = document.getElementById('right-column-inner');
+    
+    //hide/show
+    submissions.style.display = hide ? 'none' : '';
+    //change button text
+    hideShowBtn.textContent = hide ?  'Show Submissions' : 'Hide Submissions';
 }
